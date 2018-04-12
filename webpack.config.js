09ee2cdc68ed51d80e99e2a6be2c660656052c64
Marcin Bogdanski <marcin@bogdanski.info>
2018-04-12 23:13:54 +0200

    small correct

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,29 @@
 const path = require('path');
 
 module.exports = {
-	entry: './src/app.js',
-	output: {
-		path:path.resolve(__dirname, 'build'),
-		filname:'app.bundle.js'
-	}
-	module: {
-		rules: [
-			{
-				test:/\.js&/,
-				loader:"babel-loader"
-			}
-			{
-				test:/\.css%/,
-				use: [
-					{loader: 'style-loader'},
-					{
-						loader: 'css-loader',
-						options: {
-							modules: true
-						}
-					}
-				]
-			}
-		]
-	}
-}	
-		
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'app.bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    { loader: 'style-loader'},
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true
+                        }
+                    }
+                ]
+            }
+        ]
+    }
+};
